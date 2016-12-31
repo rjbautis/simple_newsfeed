d41c5ca58dcc8a9b7aa5b3518c19397cf7f2575d
Ryan Bautista <rjbautis1@gmail.com>
2016-12-31 13:37:39 -0800

    Added "help" option
    When user inputs the word “help,” the expected output is the 70 sources
    available from NewsAPI

--- simple_newsfeed.py
+++ simple_newsfeed.py
@@ -15,21 +15,29 @@ your_api_key = '5f8c9be7dc664fa4a3ee8311ce5322b6'
 random_source = random.sample(sources_dict.keys(), 3)
 
 print("Find and read today's top headlines from 70 sources!")
+
 print("For example, you can choose from", end=' ')
+
 for i in range(len(random_source)):
     print(random_source[i], end=', ')
+
 print("etc.")
 
-user_input = input('What news source would you like to see? ')
+user_input = input('What news source would you like to see? Type "help" to see full list. \n')
 
 while True:
-    try:
-        source_query = sources_dict[user_input]
-        break
-    except KeyError:
-        user_input = input('That is not a valid news source. Enter again: ')
+    if(user_input == "help"):
+        for keys in sources_dict.keys():
+            print(keys)
+        user_input = input('What news source would you like to see? ')
         continue
-
+    else:
+        try:
+            source_query = sources_dict[user_input]
+            break
+        except KeyError:
+            user_input = input('That is not a valid news source. Enter again: ')
+            continue
 
 url = 'https://newsapi.org/v1/articles?source=' + source_query + '&sortBy=top&apiKey=' + your_api_key
 
