17797f446d03d621a4179baba4b3bbcd842c4d73
Ryan Bautista <rjbautis1@gmail.com>
2017-05-15 11:14:11 -0700

    Clean up & comments added

--- simple_newsfeed.py
+++ simple_newsfeed.py
@@ -12,15 +12,13 @@ from sources import sources_dict
 
 your_api_key = '5f8c9be7dc664fa4a3ee8311ce5322b6'
 
+print("Find and read today's top headlines from 70 sources!\nFor example, you can choose from", end=' ')
+
+# Choose 3 random keys from sources_dict dictionary from sources.py
 random_source = random.sample(sources_dict.keys(), 3)
 
-print("Find and read today's top headlines from 70 sources!")
-
-print("For example, you can choose from", end=' ')
-
 for i in range(len(random_source)):
-    print(random_source[i], end=', ')
-
+    print(random_source[i], end=', ')  # adds ', ' instead of newline 
 print("etc.")
 
 user_input = input('What news source would you like to see? Type "help" to see full list. \n')
@@ -33,9 +31,11 @@ while True:
         continue
     else:
         try:
+            # if valid, assign inputted string to new variable
             source_query = sources_dict[user_input]
             break
         except KeyError:
+            # if not, catch error and ask again
             user_input = input('That is not a valid news source. Enter again: ')
             continue
 
@@ -45,10 +45,13 @@ r = requests.get(url)
 
 parsed_json = json.loads(r.text)  # turn JSON text into readable dictionary 
 
+print(parsed_json)
+print(len(parsed_json['articles']))
+
 for i in range(0, len(parsed_json['articles'])):
     print()
     headline = parsed_json['articles'][i]['title']
-    print(str(i + 1) + ") \"" + headline + "\"")
+    print(str(i + 1) + ") " + headline )
     print("   Link:", parsed_json['articles'][i]['url'])
 
 print("\nPowered by NewsAPI: https://newsapi.org")
